2b8f2d773466270d6aab5341662bd045d67302dd
Jeff Krage <krage_jeff@yahoo.com>
2020-03-05 16:32:23 -0700

    daily update 3.5.20

---
+++ mode-1/css1-exercises/css-grid-practice/css-grid-responsive/index.js
@@ -0,0 +1,22 @@
+const numbers = [4, 2, 5, 7, 8]
+
+console.log(numbers[3]);
+
+object
+
+// const user = {
+//         name: 'Chris',
+//         age: 3,
+//         friends: [
+//             {
+//                 name: "Maria",
+//                 pets: ['dog']
+//             }
+//             {
+//                 name: 'Ann',
+//                 pets: ['cats', 'fish', 'rat', 'dog', 'horse']
+//             }
+//                 ]
+// }
+
+// console.log(user.friends[2].pets)
